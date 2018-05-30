ce90d7ff06670fe099b8c1bf3d730c9ce854438b
Blaize Kaye <blaize.kaye@gmail.com>
2018-05-30 15:40:52 +1200

    added logic to override the 'extra' file in gdprdump

--- src/DumpCommand.php
+++ src/DumpCommand.php
@@ -5,5 +5,12 @@ use machbarmacher\GdprDump\Command\DumpCommand as BaseDumpCommand;
 
 class DumpCommand extends BaseDumpCommand
 {
+    protected function getDefaults($extraFile)
+    {
+        if(empty($extraFile)) {
+            $extraFile = getcwd() . "/.gdpr";
+        }
+        return parent::getDefaults($extraFile);
+    }
 
 }
