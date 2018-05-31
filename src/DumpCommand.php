9ab09e835e906c06df102bc96a357b937231f7a5
Blaize Kaye <blaize.kaye@gmail.com>
2018-05-31 21:10:33 +1200

    working in defaults-extra-file

--- src/DumpCommand.php
+++ src/DumpCommand.php
@@ -13,16 +13,28 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DumpCommand extends BaseDumpCommand
 {
+    protected $defaultsExtraFile = null;
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->defaultsExtraFile = $input->getOption('defaults-extra-file'); //store this in us for now ...
+        return parent::execute($input, $output);
+    }
+
     protected function configure()
     {
         parent::configure();
         $this->addOption("defaults-extra-file", NULL, InputOption::VALUE_OPTIONAL, "Read this file after the global files are read.");
+        $this->addOption("ignore-table", NULL, InputOption::VALUE_OPTIONAL, "Ignore the table given.");
     }
 
     protected function getDefaults($extraFile)
     {
-        if(empty($extraFile)) {
-            $extraFile = getcwd() . "/.gdpr";
+        //TODO: this logic needs to live elsewhere now ...
+        // if(empty($extraFile)) {
+        //     $extraFile = getcwd() . "/.gdpr";
+        // }
+        if(empty($extraFile) && !empty($this->defaultsExtraFile)) {
+            $extraFile = $this->defaultsExtraFile;
         }
         return parent::getDefaults($extraFile);
     }
