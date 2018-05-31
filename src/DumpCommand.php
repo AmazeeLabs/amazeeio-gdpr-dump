ae3f5810c25782b4aca2c6dc79f0ba031daecc68
Blaize Kaye <blaize.kaye@gmail.com>
2018-05-31 20:14:24 +1200

    added defaults-extra-file argument

--- src/DumpCommand.php
+++ src/DumpCommand.php
@@ -2,9 +2,23 @@
 namespace amazee\AmazeeioGdprDump;
 
 use machbarmacher\GdprDump\Command\DumpCommand as BaseDumpCommand;
+use Ifsnop\Mysqldump\Mysqldump;
+use machbarmacher\GdprDump\ConfigParser;
+use machbarmacher\GdprDump\MysqldumpGdpr;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class DumpCommand extends BaseDumpCommand
 {
+    protected function configure()
+    {
+        parent::configure();
+        $this->addOption("defaults-extra-file", NULL, InputOption::VALUE_OPTIONAL, "Read this file after the global files are read.");
+    }
+
     protected function getDefaults($extraFile)
     {
         if(empty($extraFile)) {
